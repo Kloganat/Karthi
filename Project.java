358c926a72cd7e42f0312acb8dceb2e783f33181
kloganat <karthickloganathan1406@gmail.com>
2021-08-05 05:06:42 -0700

    Folder

---
+++ Project.java
@@ -0,0 +1,13 @@
+package org.javaa;
+
+
+public class Project {
+	public void Class() {
+	System.out.println("Class");
+}
+	public static void main(String[] args) {
+		Project suji = new Project();
+		suji.Class();
+	}
+
+}
